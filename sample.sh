c6ab0b2aa6cf5870a849442db3ae70b966417fe6
akila543 <akihems666@gmail.com>
2017-02-24 11:39:47 +0530

    firstcommit

---
+++ sample.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+error=0
+warning=0
+if [ $error -eq 0 ] && [ $warning -eq 0 ]; then 
+	git merge master
+	git checkout master
+	git merge develop
+	git push origin develop
+else
+	echo "rejected"
+fi
+# VALID_PASSWORD="secret" #this is our password.
+
+# echo "Please enter the password:"
+# read PASSWORD
+
+# if [ "$PASSWORD" == "$VALID_PASSWORD" ]; then
+# 	echo "You have access!"
+# else
+# 	echo "ACCESS DENIED!"
+# fi
